4ebd0d5d40b8b34a68bb1c2f0a13de53f3571a58
Austin Graham <150076085+agraham-oni@users.noreply.github.com>
2024-12-15 00:02:47 -0800

    collect memory metrics

--- BetterPerfMon/Services/ThreadedMetricsService.cs
+++ BetterPerfMon/Services/ThreadedMetricsService.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Diagnostics;
+using System.Management;
 using System.Threading;
 using BetterPerfMon.Models;
 
@@ -24,17 +26,38 @@ public class ThreadedMetricsService : IMetricsService
 
     private void _CollectMetrics(Action<MetricsSet> messageCallback)
     {
+        ulong totalMemory = _GetTotalPhysicalMemoryMb();
+        
+        // Get total time CPU spends executing threads of a process.
+        PerformanceCounter cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+        PerformanceCounter ramCounter = new PerformanceCounter("Memory", "Available Bytes");
+        
         while (!_stopCollecting.IsSet)
         {
+            var memGbAvailable = ramCounter.NextValue() / (1024 * 1024 * 1024);
             var metrics = new MetricsSet
             {
                 Timestamp = DateTime.Now,
-                CpuPercent = 50.0f,
-                MemoryPercent = 100.0f,
-                GpuPercent = 100.0f,
+                CpuPercent = cpuCounter.NextValue(),
+                MemoryPercent = ((totalMemory - memGbAvailable) / totalMemory) * 100,
+                MemoryGb = totalMemory - memGbAvailable
             };
             messageCallback(metrics);
             Thread.Sleep(1000);
         }
+    }
+
+    private ulong _GetTotalPhysicalMemoryMb()
+    {
+        ObjectQuery query = new ObjectQuery("SELECT TotalPhysicalMemory FROM Win32_ComputerSystem");
+        ManagementObjectSearcher searcher = new ManagementObjectSearcher(query);
+
+        foreach (ManagementObject obj in searcher.Get())
+        {
+            ulong totalPhysicalMemory = (ulong)obj["TotalPhysicalMemory"];
+            return totalPhysicalMemory / (1024 * 1024 * 1024);
+        }
+
+        return 0;
     }
 }
